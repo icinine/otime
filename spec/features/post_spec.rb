8220ebe35df4e0ff0dabcf212d9b61dde3de1c17
Shane Carr <shane.carr@sitesignals.com>
2017-02-17 12:45:19 +0000

    Refactored post spec

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,33 +2,33 @@ require 'rails_helper'
 
 describe 'navigate' do
   before do
-    user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-      login_as(user, :scope => :user)
+    @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+    login_as(@user, :scope => :user)
   end
-  
-  
+
   describe 'index' do
-  	before_do
-  	  visit_posts_path
+    before do
+      visit posts_path
+    end
+
+  	it 'can be reached successfully' do
+  		expect(page.status_code).to eq(200)
   	end
 
   	it 'has a title of Posts' do
   		expect(page).to have_content(/Posts/)
   	end
-  	
-  	it 'has a list of posts' do
-  	  post1 = Post.create(date: Date.today, rationale: "Post1")
-  	  post2 = Post.create(date: Date.today, rationale: "Post2")
-  	  	visit posts_path
-  	  	expect(page).to have_content(/Post1|Post2/)
-  	end
-  	
-  	
+
+    it 'has a list of posts' do
+      post1 = Post.create(date: Date.today, rationale: "Post1", user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
+      visit posts_path
+      expect(page).to have_content(/Post1|Post2/)
+    end
   end
 
   describe 'creation' do
   	before do
-      
   		visit new_post_path
   	end
 
@@ -44,7 +44,7 @@ describe 'navigate' do
       expect(page).to have_content("Some rationale")
   	end
 
-    it 'will have a user associated with it' do
+    it 'will have a user associated it' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "User Association"
       click_on "Save"
